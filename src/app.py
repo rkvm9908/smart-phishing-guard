b24d2037654ca328f0b88f17ca47fa6f0776f037
Mathuprasanth.R.K <148639654+rkvm9908@users.noreply.github.com>
2025-12-26 01:10:28 +0530

    Update app.py
    change . __main__ function

--- src/app.py
+++ src/app.py
@@ -386,8 +386,9 @@ def static_files(filename):
     return app.send_static_file(filename) 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
     
+
 
 
 
